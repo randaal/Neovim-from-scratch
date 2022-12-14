c1a7390ba5c4d2e433464d549132a23838680e72
Paul Brandau <randaal@users.noreply.github.com>
2022-12-14 09:02:05 -0500

    comments
    Signed-off-by: Paul Brandau <randaal@users.noreply.github.com>

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -5,7 +5,7 @@ end
 
 require("user.lsp.mason")
 require("user.lsp.handlers").setup()
---require("user.lsp.null-ls")
+--require("user.lsp.null-ls") --maybe bring this back if lsp-zero is no bueno :(
 --require("user.lsp.rust_analyzer").setup()
 
 local zero = require('lsp-zero')
